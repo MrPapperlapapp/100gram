4475b71820c881ae69500587a37637285a9de2b6
MrPapperlapapp <krivmann00@gmail.com>
2026-02-15 15:52:07 +0200

    feat: add cors

--- src/core/configs/app-setup/app-setup.config.ts
+++ src/core/configs/app-setup/app-setup.config.ts
@@ -3,6 +3,15 @@ import { pipesSetup } from "./settings";
 import { swaggerSetup } from "./settings/swagger.config";
 
 export function appSetup(app: INestApplication) {
+	app.enableCors({
+		origin: [
+			"http://localhost:3000",
+			"http://95.81.100.95",
+		],
+		credentials: true,
+		methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+		allowedHeaders: ["Content-Type", "Authorization"],
+	});
 	pipesSetup(app);
 	swaggerSetup(app);
 }
